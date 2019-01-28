9cb8a277b7de1e0cc2fa86ed75e279858c5f0da1
elaine <elaine-me@elaines-MacBook-Pro.local>
2019-01-28 12:14:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach( a => console.log(a.name));
+
+}
+
+function logDriversByHometown(array, loc) {
+  let newArray = array.filter (function (a) {
+    for (let key in a) {
+   if (a[key] == loc) {
+     return a;  
+  }
+   }
+ })
+  newArray.forEach(i => console.log(i.name));
+}
+
+function driversByRevenue(array) {
+  return [...array].sort( (a, b) => a.revenue - b.revenue);
+}
+
+function driversByName(array) {
+  return [...array].sort( (a, b) => a.name.localeCompare(b.name));
+}
+
+function totalRevenue(array) {
+return array.reduce(function (acc, curr) {
+  return acc + curr.revenue;
+}, 0);
+}
+
+function averageRevenue(array) {
+return array.reduce(function (acc, curr) {
+  return (acc + curr.revenue/array.length);
+}, 0);
+ 
+}
